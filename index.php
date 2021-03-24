24d5750891eee4dde4c4adba0b10113a2fe25353
Jonas <jonasvieirafelix@gmail.com>
2021-03-24 14:54:55 -0300

    alterGithub

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Send Mail</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+</head>
+
+<body>
+    <div class="container">
+        <div class="py-3 text-center">
+            <img src="./imagem/wing_mail.png" alt="" width="100" class="d-block mx-auto mb-2">
+            <h2>Send Mail</h2>
+            <p class="lead">Seu App de Envio de Email Particular</p>
+        </div>
+        <div class="row"></div>
+        <div class="col-md-12"></div>
+        <form action="processa_envio.php" method="POST">
+            <div class="form-group">
+                <label for="para">Para</label>
+                <input type="text" name="para" class="form-control" id="para" placeholder="joao@dominio.com">
+            </div>
+            <div class="form-group">
+                <label for="assunto">Assunto</label>
+                <input type="text" class="form-control" id="assunto" name="assunto" placeholder="Assunto da Mensagem">
+            </div>
+            <div class="form-group">
+                <label for="mensagem">Mensagem</label>
+                <textarea name="mensagem" class="form-control" id="mensagem" cols="30" rows="10"></textarea>
+            </div>
+
+            <button type="submit" class="btn btn-primary btn-lg">Enviar</button>
+
+        </form>
+
+    </div>
+</body>
+
+</html>
---
+++ processa_envio.php
@@ -0,0 +1,81 @@
+<?php
+
+require "./bibliotecas/phpMailer/Exception.php";
+require "./bibliotecas/phpMailer/OAuth.php";
+require "./bibliotecas/phpMailer/PHPMailer.php";
+require "./bibliotecas/phpMailer/POP3.php";
+require "./bibliotecas/phpMailer/SMTP.php";
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+use PHPMailer\PHPMailer\SMTP;
+
+    class Mensagem{
+        private $para = null;
+        private $assunto = null;
+        private $mensagem = null;
+
+        public function __get($atrib){
+            return $this->$atrib;
+        }
+
+        public function __set($atrib, $valor){
+            $this->$atrib = $valor;
+            
+        }
+
+        public function mesagemValida(){
+            if(empty($_POST["para"]) || empty($_POST["assunto"]) || empty($_POST["mensagem"])){
+                return false;
+            } else{
+                return true;
+            }
+        }
+    }
+
+    $mensagem = new Mensagem();
+    $mensagem ->para = $_POST["para"];
+    $mensagem ->assunto = $_POST["assunto"];
+    $mensagem ->mensagem = $_POST["mensagem"];
+    echo '<pre>';
+    print_r($mensagem);
+    echo '</pre>';
+    $mail = new PHPMailer(true);
+
+    if(!$mensagem->mesagemValida()){
+        echo "Falta Preenchimento";
+        header('Location: index.php');
+    } 
+
+    try{
+        //configuração do servidor
+        $mail->SMTPDebug = false;
+        $mail->isSMTP();
+        $mail->Host = "smtp.gmail.com";
+        $mail->SMTPAuth = true;
+        $mail->Username ="senaicodetest@gmail.com";
+        $mail->Password= "senai302";
+        $mail->SMTPSecure = "tls";
+        $mail->Port = 587;
+
+        $mail->setFrom("senaicodetest@gmail.com", "web Completo");
+        $mail->addAddress($mensagem->para);
+        /* $mail->addReplyTo();
+        $mail->addCC();
+        $mail->addBCC(); */
+
+        $mail->isHTML(true);
+        $mail->Subject= $mensagem->assunto;
+        $mail->Body = $mensagem->mensagem;
+        $mail->AltBody="é necessario bla bla bla";
+        $mail->send();
+
+        $mensagem->status['codigo_status'] = 1;
+        $mensagem->status['descricao_status'] = "Email Enviado Com Sucesso";
+
+    }catch(Exception $e){
+        $mensagem->status['codigo_status'] = 2;
+        $mensagem->status['descricao_status'] = "Email não Enviado Com Sucesso" . $mail->ErrorInfo;
+        echo $e;
+    }
+?>
